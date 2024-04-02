1a5409bc719824ad6f6645f3468853e81d3932b2
cpolish <68736710+cpolish@users.noreply.github.com>
2024-04-02 11:25:20 +1100

    add Python quine

---
+++ quine.py
@@ -0,0 +1,22 @@
+CODE = """
+
+def main() -> None:
+    print('CODE = {0}{0}{0}{1}{0}{0}{0}'.format(chr(34, CODE))
+    
+    # Print remainder of code
+    print(f'{CODE}')
+
+
+if __name__ == '__main__':
+    main()"""
+
+
+def main() -> None:
+    print('CODE = {0}{0}{0}{1}{0}{0}{0}'.format(chr(34), CODE))
+    
+    # Print remainder of code
+    print(f'{CODE}')
+
+
+if __name__ == "__main__":
+    main()
